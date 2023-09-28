f2f7250cd9a61c77d259b5fef3acfa91dbd5420d
egaotan <chain_dev@163.com>
2023-09-28 20:02:04 +0800

    update

--- eth_transfer_by_contract/contracts/EthTransfer.sol
+++ eth_transfer_by_contract/contracts/EthTransfer.sol
@@ -1,8 +1,19 @@
-
+// SPDX-License-Identifier: MIT
 pragma solidity > 0.8.0;
 
 contract EthTransferInternal {
-    function execution(address payable _to) external payable {
+    address public owner;
+    constructor() {
+    }
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+    function setOwner(address _owner) external {
+        require(owner == address(0));
+        owner = _owner;
+    }
+    function execution(address payable _to) external payable onlyOwner {
         _to.call{value: msg.value}("");
         require(false, "revert");
     }
@@ -10,9 +21,18 @@ contract EthTransferInternal {
 
 contract EthTransferExternal {
     event Log(string message);
-    function execution(address proxy, address payable _to) external payable {
-        EthTransferInternal ethTransfer = EthTransferInternal(proxy);
-        try ethTransfer.execution{value: msg.value}(_to) {
+    address public owner;
+    EthTransferInternal public ethTransferInternal;
+    constructor(address _ethTransferInternal) {
+        owner = msg.sender;
+        ethTransferInternal = EthTransferInternal(_ethTransferInternal);
+    }
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+    function execution(address payable _to) external payable onlyOwner {
+        try ethTransferInternal.execution{value: msg.value}(_to) {
             emit Log("call successful");
         } catch {
             emit Log("call failed");
